ff9dff36ec8262a920091b069a9f34633741e568
melanie-farrugia <mel.farrugia@posteo.de>
2026-04-23 17:26:22 +0200

    fix PWA without plugin

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'My Collections',
-        short_name: 'Collections',
-        description: 'Save and organise your recipes and patterns',
-        theme_color: '#0d3349',
-        background_color: '#f0f7f9',
-        display: 'standalone',
-        icons: [
-          {
-            src: 'icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'icon-512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
-  ],
+  plugins: [react()],
 })
